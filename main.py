5ff9947d0748de71c872b2e2aa6b9f13edb69882
Trevor Sullivan <trevortds3@gmail.com>
2021-10-29 19:59:40 -0600

    exit more gracefully, remove log messages except username

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
 server_id = int(os.getenv('SERVER_ID'))
-print(server_id)
 bot_test = int(os.getenv('CHANNEL_ID'))
 role_id = int(os.getenv('ROLE_ID'))
 
@@ -24,7 +23,7 @@ logString = sys.argv[1]
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    # print(f'{client.user} has connected to Discord!')
 
     server = discord.utils.find(lambda g: g.id == server_id, client.guilds)
     # for guild in client.guilds:
@@ -35,21 +34,20 @@ async def on_ready():
     #     if guild.id == server_id:
     #         server = guild
 
-    print(server.id)
-    print(server.channels)
+    # print(server.id)
+    # print(server.channels)
 
     channel = discord.utils.find(lambda x: x.id == bot_test, server.channels)
-    print(channel)
-    print(server.roles)
+    # print(channel)
+    # print(server.roles)
     role = discord.utils.find(lambda x : x.id == role_id, server.roles)
-    print(role)
-    print(logString)
+    # print(role)
+    # print(logString)
     if match := re.search(r"Join succeeded: (.*)", logString):
         print(match.groups()[0])
         await channel.send(f"{role.mention} {match.groups()[0]} just logged in!")
 
-    exit(0)
-
+    await client.close()
 
 if __name__ == "__main__":
 
